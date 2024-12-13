9cdfc5064a46416ea71ba8614848d27a187ebf67
rassulya <rasul.yermagambet@nu.edu.kz>
2024-12-13 11:20:40 +0000

    add definition of local models in quantization

--- src/quantization/main.py
+++ src/quantization/main.py
@@ -6,16 +6,13 @@ from awq import AutoAWQForCausalLM
 from transformers import AutoTokenizer
 from datasets import load_dataset
 from pathlib import Path
-import yaml  # Assuming you need to parse YAML
+import yaml  
 
-# Get the root directory of the project
 ROOT_DIR = Path(os.getenv("PROJECT_ROOT", "."))
 
-# Path to the configuration file
 CONFIG_PATH = ROOT_DIR / "conf" / "parameters_quantization.yaml"
 
 
-# Load the configuration
 def load_config():
     with open(CONFIG_PATH, "r") as file:
         config = yaml.safe_load(file)
@@ -35,7 +32,7 @@ os.environ['CUDA_VISIBLE_DEVICES'] = cuda_visible_devices  # Ensure only GPUs 4-
 
 # Create a descriptive folder name based on the quantization config
 folder_name = (
-    f"Llama-3.1-8b-instruct-kk_bench_calibrated"
+    f"{model_path}"
     f"_AWQ-{quant_config['w_bit']}bit"
     f"-g{quant_config['q_group_size']}"
     f"-{quant_config['version'].lower()}"
@@ -48,6 +45,10 @@ output_path.parent.resolve().mkdir(parents=True, exist_ok=True)
 
 print(f"Quantized model will be saved to: {output_path}")
 
+
+model_path = Path(model_path).expanduser().resolve().absolute()
+
+
 # Load the original model and tokenizer
 model = AutoAWQForCausalLM.from_pretrained(
     model_path,
@@ -58,18 +59,17 @@ model = AutoAWQForCausalLM.from_pretrained(
     low_cpu_mem_usage=True
 )
 tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
-    
+
 # Print model distribution
 print("Model distribution across GPUs:")
 for name, param in model.named_parameters():
     print(f"{name}: {param.device}")
 
-
 # Initialize and move meta tensors to CUDA devices
 def initialize_and_move_meta_to_cuda(model, device_ids):
-    # available_devices = list(range(torch.cuda.device_count()))
-    # if not set(device_ids).issubset(set(available_devices)):
-    #     raise ValueError(f"Some device IDs in {device_ids} are not available. Available devices: {available_devices}")
+    available_devices = list(range(torch.cuda.device_count()))
+    if not set(device_ids).issubset(set(available_devices)):
+        raise ValueError(f"Some device IDs in {device_ids} are not available. Available devices: {available_devices}")
 
     for name, param in model.named_parameters():
         if param.device == torch.device("meta"):
@@ -79,21 +79,22 @@ def initialize_and_move_meta_to_cuda(model, device_ids):
             param.data = param.to(device)
             device_ids.append(device.index)  # Append the device ID back to the end of the list
 
-# initialize_and_move_meta_to_cuda(model, [0, 1, 2, 3])  # Use indices 0, 1, 2, 3 for GPUs 4, 5, 6, 7
+# Move meta tensors to available GPUs
+initialize_and_move_meta_to_cuda(model, [0, 1, 2, 3])  # Use indices 0, 1, 2, 3 for GPUs 4, 5, 6, 7
 
 # Enable gradient checkpointing to save memory (if supported by the model)
 if hasattr(model, 'gradient_checkpointing_enable'):
     model.gradient_checkpointing_enable()
 
 def load_kk_calib():
-    data = load_dataset('akylbekmaxutov/kazakh_calibration_dataset', split="train")
+    data = load_dataset('akylbekmaxutov/KK_calibration_dataset', split="train")
     return [text for text in data["text"]]
 
 # Quantize the model
 model.quantize(
     tokenizer,
     quant_config=quant_config,
-    #calib_data=load_kk_calib()
+    #calib_data=load_kk_calib(),
 )
 
 # Save the quantized model and tokenizer
