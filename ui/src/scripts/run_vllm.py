42c66d192984c94a2f7b7f393330a07ed80f8cc0
rassulya <rasul.yermagambet@nu.edu.kz>
2024-12-18 12:51:11 +0000

    set credentials for model run

--- ui/src/scripts/run_vllm.py
+++ ui/src/scripts/run_vllm.py
@@ -1,5 +1,7 @@
 import sys
 from pathlib import Path
+from huggingface_hub import login
+import yaml
 
 # Add the src directory to Python path
 src_dir = Path(__file__).parent.parent
@@ -7,5 +9,12 @@ sys.path.append(str(src_dir))
 
 from services.vllm import vllm_server
 
+def login_hf():
+    with open("conf/credentials.yaml") as f:
+        credentials = yaml.safe_load(f)
+    token = credentials['hf_token']
+    login(token)
+
 if __name__ == "__main__":
+    login_hf()
     vllm_server.start_server()
