801793349d4dc0dcd806d89173a4fe658cbaa711
rassulya <rasul.yermagambet@nu.edu.kz>
2024-12-27 07:47:25 +0000

    update to include torchtune packages data and models

---
+++ src/training/torchtune/data/__init__.py
@@ -0,0 +1,50 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+from torchtune.data._chat_formats import ChatFormat
+from torchtune.data._common import CROSS_ENTROPY_IGNORE_IDX
+from torchtune.data._converters import get_openai_messages, get_sharegpt_messages
+from torchtune.data._instruct_templates import InstructTemplate, AlpacaInstructTemplate
+from torchtune.data._messages import (
+    ChosenRejectedToMessages,
+    InputOutputToMessages,
+    JSONToMessages,
+    Message,
+    Role,
+    ShareGPTToMessages,
+)
+from torchtune.data._prompt_templates import (
+    ChatMLTemplate,
+    GrammarErrorCorrectionTemplate,
+    PromptTemplate,
+    PromptTemplateInterface,
+    QuestionAnswerTemplate,
+    SummarizeTemplate,
+)
+from torchtune.data._utils import truncate, validate_messages
+
+__all__ = [
+    "AlpacaInstructTemplate",
+    "ChatFormat",
+    "CROSS_ENTROPY_IGNORE_IDX",
+    "GrammarErrorCorrectionTemplate",
+    "InstructTemplate",
+    "SummarizeTemplate",
+    "JSONToMessages",
+    "ShareGPTToMessages",
+    "truncate",
+    "Message",
+    "validate_messages",
+    "Role",
+    "PromptTemplateInterface",
+    "PromptTemplate",
+    "InputOutputToMessages",
+    "ChosenRejectedToMessages",
+    "QuestionAnswerTemplate",
+    "ChatMLTemplate",
+    "get_openai_messages",
+    "get_sharegpt_messages",
+]
